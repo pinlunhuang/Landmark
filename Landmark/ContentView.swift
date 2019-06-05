f87d5e9f6ced2222264bec194df99f25fe1a6127
pinlun <pudchi330@gmail.com>
2019-06-05 17:22:17 +0800

    Initial Commit

---
+++ Landmark/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by 黃品綸 on 2019/6/5.
+//  Copyright © 2019 Pinlun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
